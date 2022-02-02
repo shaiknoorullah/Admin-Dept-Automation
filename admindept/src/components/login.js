c7b314d072775e3f2c86538a074435d4895f4806
Devsed-official <devsedofficial@gmail.com>
2022-02-03 04:59:40 +0530

    Initial login UI

--- admindept/src/components/login.js
+++ admindept/src/components/login.js
@@ -1,18 +1,32 @@
 import React from 'react';
-import Bg from '../img/bg.svg'
+import Top from '../img/top.svg'
 
 export default function login() {
   return <div>
-      <div className='absolute px-4 text-white h-full '>
-      <div className='text-center w-1/2 font-gilroy font-semibold text-lg'>
-      Made for students, By the students.
-      </div>
-      <div className='w-1/2 text-center font-sofiapro font-light text-[#cfddff]'>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed aliquam nunc, rhoncus nibh vitae at nulla ultricies. Tempor, in integer egestas adipiscing pharetra vestibulum
-      </div>
-      </div>
-      <div>
-      <img src={Bg} alt=""  className='h-screen'/>
+    {/*Left Column*/}
+      <div className='flex flex-row'>
+      <div className='bg-[#2455D6] basis-1/2 h-screen flex justify-center items-center'>
+              <img src={Top} alt="" className='w-4/5'/>
+          </div>
+           {/*right Column*/}
+          <div className='px-14 grid grid-flow-row gap-0 row-span-2'>
+              {/*intro Text*/}
+              <div className='grid content-center'>
+              <h2 className='font-gilroy font-semibold text-2xl'>Let’s Login, Shall we?
+              <span className='font-sofiapro font-light opacity-70 block text-lg'>
+                A roll no here and a mobile number there and login.<br />Easee-peezee
+              </span>
+              </h2>
+              </div>
+              {/*form*/}
+              <div className='grid content-start row-span-2'>
+                  <form action="" className='max-w-md mr-auto'>
+                      <label htmlFor="" className='block py-2'>Roll No:</label>
+                      <input type="text" placeholder='Enter Your Roll Number' className='pl-6 pr-44 py-[9px] border-2 rounded-md max-w-md'/>
+                      <button className='block mt-8 bg-[#4165BF] max-w-md mx-auto px-40 py-[9px] rounded-md text-white'>Get OTP</button>
+                  </form>
+              </div>
+          </div>  
       </div>
   </div>;
 }
