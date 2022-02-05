3b91b9a0be8f8b2974089f579615069552801da8
Devsed-official <devsedofficial@gmail.com>
2022-02-05 12:28:44 +0530

    validation styling

--- admindept/src/components/login.js
+++ admindept/src/components/login.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Top from '../img/top.svg'
+import {validation} from './validate'
 
 export default function login() {
  
@@ -23,7 +24,7 @@ export default function login() {
               <div className='grid content-start row-span-2'>
                   <form action="" className='max-w-md mr-auto'>
                       <label htmlFor="" className='block py-2'>Phone Number:</label>
-                      <input type="tel" name="phone" placeholder='Enter Your Number' defaultValue={"+"+91} className='pl-6 pr-44 py-[9px] border-2 rounded-md max-w-md'/>
+                      <input type="tel" name="phone" id='input' placeholder='Enter Your Number' defaultValue={"+"+91} className='pl-6 pr-44 py-[9px] border-2 rounded-md max-w-md focus:outline-none' onChange={validation}/>
                       <button className='block mt-8 bg-[#4165BF] max-w-md mx-auto px-40 py-[9px] rounded-md text-white' >Get OTP</button>
                   </form>
               </div>
--- admindept/src/components/validate.js
+++ admindept/src/components/validate.js
@@ -3,3 +3,19 @@ import React from 'react';
 export default function validate() {
   return <div></div>;
 }
+export function validation (val) {
+    
+    let phoneCheck = val.target.value
+    let input=document.getElementById('input');
+    let numberPattern = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/
+
+    if(numberPattern.test(phoneCheck))
+    {
+        input.classList.add('border-green-500')
+    }
+    else
+    {
+        input.classList.add('border-red-600')
+    }
+
+}
