bdb764198c17d42170163570dc97b123fc5ee901
Devsed-official <devsedofficial@gmail.com>
2022-02-05 12:56:49 +0530

    validation complete

--- admindept/src/components/validate.js
+++ admindept/src/components/validate.js
@@ -6,16 +6,18 @@ export default function validate() {
 export function validation (val) {
     
     let phoneCheck = val.target.value
-    let input=document.getElementById('input');
+    let button=document.getElementById('button')
     let numberPattern = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/
 
     if(numberPattern.test(phoneCheck))
     {
         val.target.classList.add('border-green-500')
+        button.disabled=false;
     }
     else
     {
         val.target.classList.add('border-red-600')
+        button.disabled=true;
     }
 
 }
