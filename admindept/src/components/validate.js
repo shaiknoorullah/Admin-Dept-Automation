6f0dc443e31ee9472a616bf40369cf79b7912326
Shaik Noorullah Shareef <shaiknooru247@gmail.com>
2022-02-14 10:06:43 +0530

    final commit before deployment

--- admindept/src/components/validate.js
+++ admindept/src/components/validate.js
@@ -1,25 +1,19 @@
-import React from 'react';
+import React from "react";
 
 export default function validate() {
   return <div></div>;
 }
-export function validation (val) {
-    
-    let phoneCheck = val.target.value
-    let input = document.getElementById('input');
-    let button=document.getElementById('recaptcha-container')
-    let numberPattern = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/
 
-    if(numberPattern.test(phoneCheck))
-    {
-        button.disabled=false;
-    }
-    else
-    {
-        val.target.classList.add('border-red-600')
-        button.disabled=true;
-    }
+export function validation(val) {
+  let phoneCheck = val.target.value;
+  let input = document.getElementById("input");
+  let button = document.getElementById("recaptcha-container");
+  let numberPattern = /^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/;
 
+  if (numberPattern.test(phoneCheck)) {
+    button.disabled = false;
+  } else {
+    val.target.classList.add("border-red-600");
+    button.disabled = true;
+  }
 }
-
-
