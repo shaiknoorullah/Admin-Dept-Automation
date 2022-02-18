e6e40825b8440a3f3755b50a418ec25a8e0867e1
Shaik Noorullah Shareef <shaiknooru247@gmail.com>
2022-02-18 11:55:50 +0530

    changes in query modal

--- admindept/src/components/query.js
+++ admindept/src/components/query.js
@@ -1,6 +1,5 @@
 import React from "react";
 import {
-<<<<<<< HEAD
   Modal,
   ModalOverlay,
   ModalContent,
@@ -16,37 +15,13 @@ import {
   Flex,
   Select,
   FormControl,
+  Stack,
 } from "@chakra-ui/react";
-=======
-    Modal,
-    ModalOverlay,
-    ModalContent,
-    ModalHeader,
-    ModalFooter,
-    ModalBody,
-    HStack,
-    ModalCloseButton,
-    Input,
-    Box,
-    Button,
-    Center,
-    Flex,
-    Select, 
-    FormControl,
-    Stack
-  } from "@chakra-ui/react";
-
-
->>>>>>> 1fab867ec1a55dfb8802219e5ca790afd9f6cd02
 
 export default function Query(props) {
   return (
     <div>
-<<<<<<< HEAD
-      <Modal isOpen={props.isOpen} size={"lg"}>
-=======
-        <Modal isOpen={props.isOpen} size={'lg'} closeOnOverlayClick={true}>
->>>>>>> 1fab867ec1a55dfb8802219e5ca790afd9f6cd02
+      <Modal isOpen={props.isOpen} size={"lg"} closeOnOverlayClick={true}>
         <ModalOverlay />
         <ModalContent>
           <ModalHeader py={"6"}>
@@ -55,19 +30,20 @@ export default function Query(props) {
 
           <ModalBody>
             <Center>
-<<<<<<< HEAD
               <Flex flexDirection={"column"}>
-                <FormControl pl={"14"}>
+                <FormControl onSubmit={props.handleSubmit} pl={"14"}>
                   <Select
                     onChange={props.handleChange}
                     placeholder="Purpose"
                     marginBottom={"8"}
                     width={"sm"}
                   >
-                    <option value="option1">Document</option>
+                    <option value="option1">Document/Scholarship</option>
                     <option value="option2">Fee Payment</option>
                   </Select>
-                  <label>Message</label>
+                  <Stack display={"block"}>
+                    <label>Message</label>
+                  </Stack>
                   <Input
                     onChange={props.handleChange}
                     placeholder="Enter Message"
@@ -77,37 +53,12 @@ export default function Query(props) {
                   />
                   <ModalFooter>
                     <Center width={"md"}>
-                      <Button
-                        colorScheme="blue"
-                        mr={3}
-                        onClick={props.handleSubmit}
-                      >
+                      <Button colorScheme="blue" mr={3}>
                         Submit
                       </Button>
                     </Center>
                   </ModalFooter>
                 </FormControl>
-=======
-              <Flex flexDirection={'column'}>
-                  <FormControl onSubmit={props.handleSubmit} pl={'14'}>
-
-                <Select onChange={props.handleChange} placeholder='Purpose' marginBottom={'8'} width={'sm'}>
-                    <option value='option1'>Document/Scholarship</option>
-                    <option value='option2'>Fee Payment</option>
-                    </Select>
-                    <Stack display={'block'}>
-                    <label>Message</label>
-                    </Stack>
-                    <Input onChange={props.handleChange} placeholder='Enter Message' size='md' width={'sm'} mb={'4'}/>
-                    <ModalFooter>
-                        <Center width={"md"}>
-                        <Button colorScheme="blue" mr={3} >
-                            Submit
-                        </Button>
-                        </Center>
-                    </ModalFooter>
-                  </FormControl>
->>>>>>> 1fab867ec1a55dfb8802219e5ca790afd9f6cd02
               </Flex>
             </Center>
           </ModalBody>
